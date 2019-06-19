ac0ca6ec7aa4fb5f494fcee0ddbdbb590457f21f
Anwesh43 <anweshthecool0@gmail.com>
2019-06-19 08:54:14 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedstepleftlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepleftlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedstepleftlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.steplineview.StepLeftLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepLeftLineView.create(this)
     }
 }

78b169906589a14c4b7801901a4c27eb48778f69
Anwesh43 <anweshthecool0@gmail.com>
2019-06-19 08:17:00 +0530

    adding libraries and constants

--- steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
+++ steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.steplineview
 /**
  * Created by anweshmishra on 19/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")

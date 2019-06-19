7d7e3d4069e30a821ca61b68fa2d011144c97944
Anwesh43 <anweshthecool0@gmail.com>
2019-06-19 08:41:21 +0530

    adding StepLeftLine class

--- steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
+++ steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
@@ -173,4 +173,28 @@ class StepLeftLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepLeftLine(var i : Int) {
+
+        private val root : SLLNode = SLLNode(0)
+        private var curr : SLLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

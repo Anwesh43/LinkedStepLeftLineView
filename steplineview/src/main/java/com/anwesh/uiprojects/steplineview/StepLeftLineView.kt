217f24de4842ae3fb4ce327e8d97cbfb2d9ebcf9
Anwesh43 <anweshthecool0@gmail.com>
2019-06-19 08:39:35 +0530

    adding SLLNode class

--- steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
+++ steplineview/src/main/java/com/anwesh/uiprojects/steplineview/StepLeftLineView.kt
@@ -129,4 +129,48 @@ class StepLeftLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLLNode(var i : Int, val state : State = State()) {
+
+        private var next : SLLNode? = null
+        private var prev : SLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLLNode {
+            var curr : SLLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
